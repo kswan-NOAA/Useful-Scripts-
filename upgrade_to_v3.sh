445fbce27fe259eaff79a8ee9fd91e48ed5a9224
kswan-NOAA <kenneth.swan@NOAA.gov>
2023-06-29 15:59:53 -0500

    NetCDF Upgrader

---
+++ upgrade_to_v3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# Created by: Alex Swan
+# 6-29-23
+# Takes an input single ob .yaml, converts to netCDF, and upgrades to v3.
+
+
+
+module use /work/noaa/da/kswan/gdasapp/modulefiles/
+module load GDAS/orion
+
+# Set python path and create ob file
+export PYTHONPATH=$PYTHONPATH:/work/noaa/da/kswan/gdasapp/iodaconv/src
+/work/noaa/da/kswan/gdasapp/build/bin/gen_single_ob.py -y singleob.yaml
+
+# Upgraders
+/work/noaa/da/kswan/gdasapp/build/bin/ioda-upgrade-v1-to-v2.x singleob.nc singleob_v2.nc
+/work/noaa/da/kswan/gdasapp/build/bin/ioda-upgrade-v2-to-v3.x singleob_v2.nc singleob_v3.nc /work/noaa/da/kswan/gdasapp/ioda/share/ioda/yaml/validation/ObsSpace.yaml
+
+# Optional
+cp ./singleob_v3.nc ./obs/singleob_v3.nc
+
