4316b2722e749a02fe61e4987edf4aa33df1dffa
kswan-NOAA <kenneth.swan@NOAA.gov>
2023-07-06 10:04:12 -0500

    Improved arrangement of tasks and added comments

--- run_tasks.sh
+++ run_tasks.sh
@@ -7,88 +7,68 @@
 #SBATCH --cpus-per-task=1
 #SBATCH -t 25:00
 
-#Directory
-
+# *CHANGE: Working Directory
 workdir=/work/noaa/da/kswan
 
-# *CHANGE*: Output Directories
-
+# *CHANGE: Output Directory and Input File
 dir1="T2.4"
 obfile="singleob.yaml"
-mkdir $workdir/jeditestrun/analysis/20210801T00000000Z/$dir1 
+mkdir $workdir/jeditestrun/analysis/20210801T00000000Z/$dir1
+ 
 # Load Modules
-
 echo "Loading modules into $workdir"
-
 cd $workdir
-
 module load intel
 module use $workdir/gdasapp/modulefiles
 module load GDAS/orion
 module load ncview
-
 cd jeditestrun/
 
 # Upgrade our single ob file before assimilation
 
 echo "Upgrading $obfile"
+upgrade_to_v3.sh $obfile
 
-#./upgrade_to_v3.sh $obfile
-
+# Perform single var incremental assimilation using 3DVar
 echo "Starting srun..."
+srun -n 216 --exclusive -t 25:00 fv3jedi_var.x sample_var.yaml &> output.log
 
-#Perform single var incremental assimilation using 3DVar
-
-#srun -n 216 --exclusive -t 25:00 fv3jedi_var.x sample_var.yaml &> output.log
-
+# Move the increment netCDF files to one directory
 cd $workdir/jeditestrun/analysis
-
-#Move the increment netCDF files to one directory
-
 echo "Moving files to $dir1"
-#./test_run.loc_to_dir.sh $dir1
+./test_run.loc_to_dir.sh $dir1
 
-#Run eva to produce plots
-
-cd $workdir
-module load EVA/orion
-
+# Creates a copy of a templated .yaml file, and uses a token to set output to the directory
 cp testCubedSphereRestart_templated.yaml testCubedSphereRestart.yaml
-
-
 find_dir="@dir@"
-#replace=$dir1
-
 sed -i "s/$find_dir/$dir1/g" testCubedSphereRestart.yaml
 
+# Load modules and run EVA
+cd $workdir
+module load EVA/orion
 echo "Evaluating testCubedSphereRestart.yaml, outputting to $dir1"
-
 eva testCubedSphereRestart.yaml
-
 #rm testCubedSphereRestart_tmp.yaml
 
+# OPTIONAL: Copy output plots and logs to the plots directory
 cd $workdir/jeditestrun/analysis/20210801T00000000Z/$dir1
-
 mkdir $workdir/jeditestrun/plots/"$dir1""_plots"
-
 for png in $(ls *.png); do
     
     cp $png $workdir/jeditestrun/plots/"$dir1""_plots"/$png
-
 done
-
 cp output.log $workdir/jeditestrun/plots/"$dir1""_plots"
-
 cd $workdir/jeditestrun/plots/
-ls
+
+
+# OPTIONAL: Upload changes to a github repository
 dir2="$dir1""_plots"
+repo="https://github.com/kswan-NOAA/Single-Obs-Experiment-Data.git"
 
 git init
-
 git add $dir2/\*
 git commit -m "Added plots from $dir1"
 git branch -M main
-repo="https://github.com/kswan-NOAA/Single-Obs-Experiment-Data.git"
 git push -u origin main
 #git init
 
